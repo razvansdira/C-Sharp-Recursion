da0eb3688bd4ee439477e8524d331045ffc83834
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-30 21:14:23 +0200

    Add files via upload

---
+++ Exercise04.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise04
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("  Input a big number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" The digits are: ");
+            separ(n);
+            Console.ReadLine();
+        }
+
+        static void separ(int x)
+        {
+            if (x < 10)
+            {
+                Console.Write("{0} ", x);
+                return;
+            }
+            separ(x / 10);
+            Console.Write(" {0} ", x % 10);
+        }
+    }
+}
