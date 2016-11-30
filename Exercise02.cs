6251978d8181c361901af8acc526869cec7aefad
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-30 21:13:08 +0200

    Add files via upload

---
+++ Exercise02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise02
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("  How many numbers to print: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            print(x, 1);
+            Console.ReadLine();
+        }
+
+        static int print(int a, int b)
+        {
+            if (a < 0)
+                return b;
+            Console.Write(" {0}", a);
+            a--;
+            return print(a, b);
+        }
+    }
+}
