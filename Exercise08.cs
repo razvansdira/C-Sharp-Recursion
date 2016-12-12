0e0cf37c84b6140dc9d6325e7a01f94df9f856b6
Razvan Sdira <sdira_razvan@yahoo.com>
2016-12-12 22:13:57 +0200

    Add files via upload

---
+++ Exercise08.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise08
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            bool tf;
+
+            Console.Write(" Input a string: ");
+            s = Console.ReadLine();
+            tf = palindrom(s);
+
+            if (tf == true)
+                Console.WriteLine("The string is palindrome.");
+            else
+                Console.WriteLine("The string is not a palindrome.");
+            Console.ReadLine();
+        }
+
+        static bool palindrom(string txt)
+        {
+            if (txt.Length <= 1)
+                return true;
+            else
+            {
+                if (txt[0] != txt[txt.Length - 1])
+                    return false;
+                else
+                    return palindrom(txt.Substring(1, txt.Length - 2));
+            }
+        }
+    }
+}
