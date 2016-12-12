42981fb3cfa9e1ca3e45e621c5c3be4aa6314437
Razvan Sdira <sdira_razvan@yahoo.com>
2016-12-12 22:48:45 +0200

    Add files via upload

---
+++ Exercise12.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise12
+    {
+        static void Main(string[] args)
+        {
+            long n, m, z, lcm;
+
+            Console.Write("  Input the first number: ");
+            n = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write(" Input the second number: ");
+            m = Convert.ToInt64(Console.ReadLine());
+
+            z = gcd(n, m);
+            lcm = (n * m) / z;
+
+            Console.WriteLine("The GCD of {0} and {1} = {2} ", n, m, z);
+            Console.WriteLine("The LCM of {0} and {1} = {2}\n", n, m, lcm);
+
+            Console.ReadLine();
+        }
+
+        static long gcd(long x, long y)
+        {
+            if (y == 0)
+                return x;
+            else
+                return gcd(y, x % y);
+        }
+    }
+}
