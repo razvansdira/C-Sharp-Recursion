e59d5b619d6aaccb9a9215c4ea321bad1212fca7
Razvan Sdira <sdira_razvan@yahoo.com>
2016-12-12 22:21:47 +0200

    Add files via upload

---
+++ Exercise09.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise09
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Input a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            long f = factorial(n);
+
+            Console.Write("The factorial is: {0}.", f);
+            Console.ReadLine();
+        }
+
+        static long factorial(int x)
+        {
+            if (x == 0)
+                return 1;
+            return x * factorial(x - 1);
+        }
+    }
+}
