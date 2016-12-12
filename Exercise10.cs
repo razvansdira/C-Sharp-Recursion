293a01d5d19346fa954d3927e794bbe6b9056566
Razvan Sdira <sdira_razvan@yahoo.com>
2016-12-12 22:28:25 +0200

    Add files via upload

---
+++ Exercise10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Recursion
+{
+    class Exercise10
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("  Input number of terms for the Fibonacci series: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The Fibonacci series of {0} terms is: ", a);
+            for (int i = 0; i < a; i++)
+                Console.Write("{0} ", fibo(i));
+            Console.ReadLine();
+        }
+
+        public static int fibo(int x)
+        {
+            int m = 0;
+            int n = 1;
+            for (int i = 0; i < x; i++)
+            {
+                int temp = m;
+                m = n;
+                n = temp + n;
+            }
+            return m;
+        }
+    }
+}
